4148187e4d4fa584531893d03df1936a9f008e23
Deva Asirvatham SJ <devaasirvathamsj@gmail.com>
2026-05-16 13:30:42 +0530

    Update main.go

--- app/main.go
+++ app/main.go
@@ -2,26 +2,25 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"os"
 )
 
 func main() {
-
 	appName := os.Getenv("APP_NAME")
 	appVersion := os.Getenv("APP_VERSION")
 
-	// Serve HTML
 	http.Handle("/", http.FileServer(http.Dir("./static")))
-
 	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "OK")
 	})
-
 	http.HandleFunc("/version", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "App: %s, Version: %s", appName, appVersion)
 	})
 
 	fmt.Println("Server running on port 8080...")
-	http.ListenAndServe(":8080", nil)
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
 }
